eb7f86048406b0efe767a8cba04cd79325aacf16
noodb <li_noodles@163.com>
2016-12-15 00:34:39 +0800

    更新
    1. 完成课程列表模板
    2. 区域二变为广告栏，放到主体里面
    3. 增加主体背景颜色

--- 网易前端微专业大作业/js/template.js
+++ 网易前端微专业大作业/js/template.js
@@ -1,1 +1,9 @@
-//模板
+/*
+*	课程列表模板
+*	对应 m-list 的 列表项中的 innerHTML 内容
+*/
+M_LIST = '<div class="img-wrap"><img src="" width="223px" height="124px"></img></div>\
+		  <p class="name" title=""></p>\
+		  <p class="provider"></p>\
+		  <div class="icon"><span class="learner"></span></div>\
+		  <em class="price"></em>';
